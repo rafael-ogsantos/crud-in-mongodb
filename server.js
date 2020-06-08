07897c9322dc34fee9be5f83e7dc697a38b5a991
Rafael Oliveira <rafaelogsantos@gmail.com>
2020-06-08 00:14:58 -0300

    first contact with mongodb

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  process.env.CONNECTIONSTRING,
+  { useUnifiedTopology: true,
+    useNewUrlParser: true }
+);
+
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3000);
